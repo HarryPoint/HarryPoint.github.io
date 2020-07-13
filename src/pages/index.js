c20c6ca45a08b87d8e8b9df53e30e4dc9cc980ca
Harry <1751826104@qq.com>
2020-07-13 21:56:07 +0800

    success

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,7 +74,7 @@ function Home() {
               )}
               to={useBaseUrl("docs/")}
             >
-              Get Started 4
+              Get Started 5
             </Link>
           </div>
         </div>
