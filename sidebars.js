0f8f48484e22f9969edc3e519f975a7d7fe7ef59
Harry <1751826104@qq.com>
2020-07-14 09:47:53 +0800

    修改

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ["doc1", "doc2", "doc3"],
-    Features: ["mdx"],
     js: ["js/数组方法", "js/对象方法"],
     resource: ["resource/list"],
     git: ["git/cli"],
+    Docusaurus: ["doc1", "doc2", "doc3"],
+    Features: ["mdx"],
   },
 };
