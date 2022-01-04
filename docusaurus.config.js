73c01ca83f7f72df2ffddedfcc8afb6f6d3acf98
HarryPoint <1751826104@qq.com>
2022-01-04 15:38:39 +0800

    feat: add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,6 +116,21 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ],
 };
 
 module.exports = config;
