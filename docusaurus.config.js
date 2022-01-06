aefdbc072792fd8469f4a512c8cb33980f18f36e
HarryPoint <1751826104@qq.com>
2022-01-06 10:54:29 +0800

    feat: 还原搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,41 +94,21 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-      algolia: {
-        // If Algolia did not provide you any appId, use 'BH4D9OD16A'
-        appId: "QWOR5P2IG5",
-
-        // Public API key: it is safe to commit it
-        apiKey: "197e8c581910a3e2f18bcd3c7ead69c7",
-
-        indexName: "prod_NAME",
-
-        // Optional: see doc section below
-        contextualSearch: true,
-
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        // externalUrlRegex: "external\\.com|domain\\.com",
-
-        // Optional: Algolia search parameters
-        searchParameters: {},
-
-        //... other Algolia params
-      },
     }),
   plugins: [
-    // [
-    //   require.resolve("@easyops-cn/docusaurus-search-local"),
-    //   {
-    //     // ... Your options.
-    //     // `hashed` is recommended as long-term-cache of index file is possible.
-    //     hashed: true,
-    //     // For Docs using Chinese, The `language` is recommended to set to:
-    //     // ```
-    //     language: ["en", "zh"],
-    //     // ```
-    //     // When applying `zh` in language, please install `nodejieba` in your project.
-    //   },
-    // ],
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ["en", "zh"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
   ],
 };
 
