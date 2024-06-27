2f3048e45c73737799a4216b86c9cecc05e25987
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-06-27 13:56:59 +0800

    feat: 添加容错处理

--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DocItem from "@theme-original/DocItem";
 import type DocItemType from "@theme/DocItem";
 import type { WrapperProps } from "@docusaurus/types";
@@ -8,22 +8,31 @@ import GitalkComponent from "gitalk/dist/gitalk-component";
 type Props = WrapperProps<typeof DocItemType>;
 
 export default function DocItemWrapper(props: Props): JSX.Element {
+  // 使用状态来控制GitalkComponent的渲染
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // 组件挂载后设置isClient为true
+    setIsClient(true);
+  }, []);
   return (
     <>
       <DocItem {...props} />
-      <GitalkComponent
-        options={{
-          clientID: "Ov23lit1eRiiNbsLJ8xv",
-          clientSecret: "a34e6c82b96b311284d30adc5d82d258691f73b9",
-          repo: "gitalk-store", // The repository of store comments,
-          owner: "HarryPoint",
-          admin: [
-            "GitHub repo owner and collaborators, only these guys can initialize github issues",
-          ],
-          // id: location.pathname, // Ensure uniqueness and length less than 50
-          distractionFreeMode: false, // Facebook-like distraction free mode
-        }}
-      />
+      {isClient && (
+        <GitalkComponent
+          options={{
+            clientID: "Ov23lit1eRiiNbsLJ8xv",
+            clientSecret: "a34e6c82b96b311284d30adc5d82d258691f73b9",
+            repo: "gitalk-store", // The repository of store comments,
+            owner: "HarryPoint",
+            admin: [
+              "GitHub repo owner and collaborators, only these guys can initialize github issues",
+            ],
+            // id: location.pathname, // Ensure uniqueness and length less than 50
+            distractionFreeMode: false, // Facebook-like distraction free mode
+          }}
+        />
+      )}
     </>
   );
 }
