4bc6ca273f14debe1c3dd2fbfda540cc828e965e
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-06-27 15:57:06 +0800

    feat: 添加背景图片

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,12 @@ import styles from "./index.module.css";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{
+        background: 'url("https://picsum.photos/seed/picsum/1920/320?blur=2")',
+      }}
+    >
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
