1d0ed0a1a88a7caa65211f73ea5fedfc258be467
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-06-27 15:13:51 +0800

    feat: 修改gitalk

---
+++ src/components/Gitalk/index.tsx
@@ -0,0 +1,32 @@
+import "gitalk/dist/gitalk.css";
+import GitalkComponent from "gitalk/dist/gitalk-component";
+import CryptoJS from "crypto-js";
+import { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
+export default function Gitalk(props) {
+  // 使用状态来控制GitalkComponent的渲染
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // 组件挂载后设置isClient为true
+    setIsClient(true);
+  }, []);
+  return isClient ? (
+    <div className={styles.wrapper}>
+      <GitalkComponent
+        options={{
+          clientID: "Ov23lit1eRiiNbsLJ8xv",
+          clientSecret: "a34e6c82b96b311284d30adc5d82d258691f73b9",
+          repo: "gitalk-store", // The repository of store comments,
+          owner: "HarryPoint",
+          admin: ["HarryPoint"],
+          proxy:
+            "https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token",
+          id: CryptoJS.MD5(location.pathname).toString(), // Ensure uniqueness and length less than 50
+          distractionFreeMode: false, // Facebook-like distraction free mode
+        }}
+      />
+    </div>
+  ) : null;
+}
--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -2,38 +2,15 @@ import React, { useEffect, useState } from "react";
 import DocItem from "@theme-original/DocItem";
 import type DocItemType from "@theme/DocItem";
 import type { WrapperProps } from "@docusaurus/types";
-import "gitalk/dist/gitalk.css";
-import GitalkComponent from "gitalk/dist/gitalk-component";
-import CryptoJS from "crypto-js";
+import Gitalk from "@site/src/components/Gitalk";
 
 type Props = WrapperProps<typeof DocItemType>;
 
 export default function DocItemWrapper(props: Props): JSX.Element {
-  // 使用状态来控制GitalkComponent的渲染
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    // 组件挂载后设置isClient为true
-    setIsClient(true);
-  }, []);
   return (
     <>
       <DocItem {...props} />
-      {isClient && (
-        <GitalkComponent
-          options={{
-            clientID: "Ov23lit1eRiiNbsLJ8xv",
-            clientSecret: "a34e6c82b96b311284d30adc5d82d258691f73b9",
-            repo: "gitalk-store", // The repository of store comments,
-            owner: "HarryPoint",
-            admin: ["HarryPoint"],
-            proxy:
-              "https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token",
-            id: CryptoJS.MD5(location.pathname).toString(), // Ensure uniqueness and length less than 50
-            distractionFreeMode: false, // Facebook-like distraction free mode
-          }}
-        />
-      )}
+      <Gitalk />
     </>
   );
 }
