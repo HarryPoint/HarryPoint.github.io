88fa3c5da6907a3ee2cc2feb1be60a23ca0c6ff7
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-12 18:39:46 +0800

    feat: 修改

--- src/theme/DocItem/index.tsx
+++ src/theme/DocItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import DocItem from "@theme-original/DocItem";
 import type DocItemType from "@theme/DocItem";
 import type { WrapperProps } from "@docusaurus/types";
